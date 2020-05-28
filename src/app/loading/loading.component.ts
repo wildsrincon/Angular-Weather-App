33788940c1ca4229f846fca8f589d9020e64bdee
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-05-28 02:20:13 -0500

    Adding changes files component loading

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -10,11 +10,31 @@ import { loadingAnimation } from '../animations/loading.animations';
 export class LoadingComponent implements OnInit {
 
   _elements: string[] = ['#ffe5ec', '#ff80a0', '#ff2e63', '#800020', '#1a0006'];
-  elements : string[] = this._elements; 
+   public elements : string[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.set();
+  }
+
+  set() {
+    this.elements = this._elements;
+    this.scheduleNextIteration();
+  }
+
+  scheduleNextIteration() {
+    setTimeout(() => {
+      if(this.elements.length == 0) return this.set();
+
+      this.clear();
+
+    }, 100 * this._elements.length + 300)
+  }
+
+  clear() {
+    this.elements = [];
+    this.scheduleNextIteration();
   }
 
 }
