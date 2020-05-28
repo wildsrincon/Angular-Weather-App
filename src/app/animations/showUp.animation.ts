691f057515d78f27fb1dae65cc244b0db651c749
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-05-28 03:07:31 -0500

    Adding changes files animations

--- src/app/animations/showUp.animation.ts
+++ src/app/animations/showUp.animation.ts
@@ -1,5 +1,15 @@
-import { trigger, state, style, animate, transition } from '@angular/animations';
- 
+import { trigger, state, style, query, animate, transition, stagger } from '@angular/animations';
+
+export const showUpStaggered = trigger('showUpCollection', [
+    transition('* => *', [
+        query(':enter', [
+            style({ opacity: 0, transform: 'scale(0)' }),
+            stagger(200, [
+                animate(250, style({ opacity: 1, transform: 'scale(1)' }))
+            ])
+        ], {optional: true})
+    ])  
+])
 
 export const showUp = trigger('showUpElement', [
     state('in', style({ opacity: 1, transform: 'scale(1)' })),
