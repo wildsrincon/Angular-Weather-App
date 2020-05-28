5b1202a091e984d4c33576ac0e849bb0fb99a072
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-05-28 02:24:45 -0500

    Adding changes file loading.animations.ts

--- src/app/animations/loading.animations.ts
+++ src/app/animations/loading.animations.ts
@@ -3,10 +3,15 @@ import { trigger, style, animate, query, stagger, transition } from '@angular/an
 export const loadingAnimation = function(){
     return trigger('loading',[
         transition('* => *', [
-            query('enter', [
+            query(':enter', [
                 style({opacity:0}),
                 stagger(100,[
                     animate('350ms',style({opacity:1}))
+                ])
+            ],{optional:true}),
+            query(':leave', [
+                stagger(100,[
+                    animate('350ms',style({opacity:0}))
                 ])
             ],{optional:true})
         ])
